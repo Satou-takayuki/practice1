b7efbc4f9ade62b0c036970e387088c8da0e982d
Satou takayuki <t.satou.sys23@morijyobi.ac.jp>
2023-10-05 14:35:45 +0900

    Hello Worldを出力
    １回出力から１００回出力に変更

--- pra2.java
+++ pra2.java
@@ -1,5 +1,7 @@
 public class pra2 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        for(int a = 0; a <= 100; a++) {
+            System.out.println("Hello World");
+        }
     }
 }
