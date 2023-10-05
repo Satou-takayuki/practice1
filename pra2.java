d36d3c423335695eed3e608de9c20dbddbbc62d0
Satou takayuki <t.satou.sys23@morijyobi.ac.jp>
2023-10-05 14:50:37 +0900

    回数を修正

--- pra2.java
+++ pra2.java
@@ -1,6 +1,6 @@
 public class pra2 {
     public static void main(String[] args) {
-        for(int a = 0; a <= 100; a++) {
+        for(int a = 1; a <= 100; a++) {
             System.out.println("Hello World");
         }
     }
